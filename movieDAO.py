ad3912de03fef4c2db20df8eb912ce66d5a0682c
Colm <colmocaoimh@outlook.com>
2020-12-06 14:42:22 +0000

    re-hash of walkthrough with my own data

---
+++ movieDAO.py
@@ -0,0 +1,88 @@
+import mysql.connector
+
+class MovieDao: 
+    db = "" # stores database connection
+    def __init__(self):
+        self.db = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            password = '',
+            database = 'big_project'
+        )
+        #print("connection made")
+
+    
+    def create(self, movie): # creates new movie but also returns latest id
+        cursor = self.db.cursor()
+        sql = "insert into movies (title, director, year) values (%s, %s, %s)"
+        values = [
+            #movie["ISBN"],
+            movie["title"],
+            movie["director"],
+            movie["year"]     
+        ]
+        cursor.execute(sql, values)
+        self.db.commit()
+        return cursor.lastrowid # lastrowid is equal to 0. Returns if successful... I think...
+    
+    def get_all(self):
+        cursor = self.db.cursor()
+        sql = "select * from movies"
+        cursor.execute(sql)
+        results = cursor.fetchall() # comes back as tuple. We want them as dict objects
+                                    # JSON on our html page in the end
+        returnarray = []
+        #print(results)
+        for result in results:
+            result_as_dict = self.convert_to_dict(result) # result is tuple so convert to dict
+            returnarray.append(result_as_dict)
+        
+        return returnarray # will return array of dict objects
+
+
+    def find_by_id(self, id):
+            cursor = self.db.cursor()
+            sql="select * from movies where id = %s"
+            values = (id,)
+
+            cursor.execute(sql, values)
+            result = cursor.fetchone()
+            return self.convert_to_dict(result)
+    
+        
+
+    def update(self, movie):
+        cursor = self.db.cursor()
+        sql = 'update movies set title = %s, director = %s, year = %s where id = %s'
+        values = [
+            movie['title'],
+            movie['director'],
+            movie['year'],
+            movie['id']
+        ] 
+        cursor.execute(sql, values)
+        self.db.commit()
+        return movie
+
+    def delete(self, id):
+        cursor = self.db.cursor()
+        sql = 'delete from movies where id = %s'
+        values = (id,)
+        cursor.execute(sql, values)
+        return {}
+    
+    def convert_to_dict(self, result):
+        col_names = ['id', 'title', 'director', 'year']
+        movie = {}
+
+        if result:
+            for i, col_name in enumerate(col_names):
+                value = result[i]
+                movie[col_name] = value
+        
+        return movie # this will be sent straight to the html file later on
+
+
+
+
+movieDao = MovieDao()
---
+++ server.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify, request, url_for, abort
+from movieDAO import movieDao
+
+app = Flask(__name__, static_url_path='', static_folder='staticpages')
+
+
+@app.route('/')
+def index():
+    return "hello there"
+
+ 
+@app.route('/movies', methods=["GET"])
+def get_all():
+    return jsonify(movieDao.get_all())
+
+@app.route('/movies/<int:id>')
+def find_by_id(id):
+    return jsonify(movieDao.find_by_id(id))
+
+@app.route('/movies', methods=["POST"])
+def create():
+    if not request.json:
+        abort(400)
+
+    movie = {
+        #"ISBN": request.json["ISBN"],
+        "title": request.json["title"],
+        "director": request.json["director"],
+        "year": request.json["year"]
+    }
+    return jsonify(movieDao.create(movie))
+
+@app.route('/movies/<int:id>', methods=["PUT"])
+def update(id):
+    found_movie = movieDao.find_by_id(id) # goes to the database for book by ISBN. Returns data primed for update, entered by user
+    #print (found_movie)
+    if found_movie == {}:
+        return jsonify({}), 404
+    current_movie = found_movie
+    if "title" in request.json: # what exactly is request.json. data type?? WHat does it do?
+        current_movie["title"] = request.json["title"]
+    if "director" in request.json:
+        current_movie["director"] = request.json["director"]
+    if "year" in request.json:
+        current_movie["year"] = request.json["year"]
+    movieDao.update(current_movie) # book still not actually updated in database until this point here.
+    
+    return jsonify(current_movie)
+
+@app.route('/movies/<int:id>', methods=["DELETE"])
+def delete(id):
+    if id == None:
+        print("ISBN not found")
+    else:
+        movieDao.delete(id)
+        return jsonify({"done":True})
+
+if __name__ == "__main__":
+    print("hello world")
+    app.run(debug=True)
+     
