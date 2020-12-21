e153b3a6d70e845e1cfee36c8e76a898e3ca68c4
Colm <colmocaoimh@outlook.com>
2020-12-21 14:28:52 +0000

    imported CORS

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, url_for, abort
+from flask_cors import CORS
 from movieDAO import movieDao
 
 app = Flask(__name__, static_url_path='', static_folder='staticpages')
